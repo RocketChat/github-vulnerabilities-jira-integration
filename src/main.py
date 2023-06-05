42ca2b6f7bb3c0fb18e4940bff8fb262a69b3438
B. Cestari <5381475+brunobcestari@users.noreply.github.com>
2023-06-05 16:31:31 -0300

    Update main.py
    just using the best practices

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ from github_helper import create_vulnerability_list
 import jira_processor
 import sys
 
-vulnerabilities = create_vulnerability_list()
 
-for data in vulnerabilities:
+def main():
+    vulnerabilities = create_vulnerability_list()
+
+    for data in vulnerabilities:
     
-    vulnerability = jira_processor.Vulnerability(data)
-    jira_issue = vulnerability.map_jira_issues()
+        vulnerability = jira_processor.Vulnerability(data)
+        jira_issue = vulnerability.map_jira_issues()
+  
+if __name__ == '__main__':
+    main()
